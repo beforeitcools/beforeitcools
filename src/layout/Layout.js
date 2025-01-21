b85cf4f5c8911c3709aac12e6baceb919e3722c3
cjmin-n <mymemose@naver.com>
2025-01-22 00:39:05 +0900

    build netfilybuild 위한 커밋

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,6 +1,6 @@
 import Footer from "../component/Common/Footer";
 import Header from "../component/Common/Header";
-import Navbar from "../component/Common/Navbar";
+// import Navbar from "../component/Common/Navbar";
 import {Outlet} from "react-router-dom";
 
 const Layout = () => {
